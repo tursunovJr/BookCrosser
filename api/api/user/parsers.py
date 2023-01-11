15e87ccdbae0a82abb774b78cf7e17ce218911b3
ztursunov <ztursunov@ivi.ru>
2023-01-11 12:35:13 +0500

    some fixes

--- api/api/user/parsers.py
+++ api/api/user/parsers.py
@@ -1,27 +1,8 @@
 from marshmallow import Schema, fields
-from datetime import datetime
 
-
-class CoordinateSchema(Schema):
-    datetime = fields.DateTime(attribute="datetime_at", format="iso8601",
-                               required=True)
-    lat = fields.Float(attribute="latitude", required=True)
-    lon = fields.Float(attribute="longitude", required=True)
-
-
-class CoordinatesSchema(Schema):
-    coordinates = fields.List(fields.Nested(CoordinateSchema), required=True)
-
-
-class TrackSchema(Schema):
-    uuid = fields.String(attribute="track_uuid")
-
-
-class CoordinatesQuerySchema(Schema):
-    begin_datetime = fields.DateTime(format="iso8601", missing=datetime.min)
-    end_datetime = fields.DateTime(format="iso8601", missing=datetime.utcnow)
-
-
-class SensorsQuerySchema(Schema):
-    begin_datetime = fields.DateTime(format="iso8601", missing=datetime.min)
-    end_datetime = fields.DateTime(format="iso8601", missing=datetime.utcnow)
+class UserSchema(Schema):
+    id = fields.Integer(attribute="id", required=True)
+    username = fields.String(attribute="username", required=True)
+    email = fields.Email(attribute="email", required=True)
+    password = fields.String(attribute="password", required=True)
+    city = fields.String(attribute="city", required=True)
