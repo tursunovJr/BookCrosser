7a558dbee6c7996db1476c79dd766ed1e5a799ca
ztursunov <ztursunov@ivi.ru>
2023-02-25 17:14:38 +0300

    some fixes in auth api

--- api/api/auth/fields.py
+++ api/api/auth/fields.py
@@ -1,1 +1,20 @@
 from marshmallow import fields, Schema, pre_dump
+
+class UserInfoSchema(Schema):
+    id = fields.String(attribute="id")
+    username = fields.String(attribute="username")
+    email = fields.String(attribute="email")
+    city = fields.String(attribute="city")
+
+    @pre_dump
+    def group(self, data, many):
+        print("Serik", data)
+        return {
+            "id": data["id"],
+            "username": data["username"],
+            "email": data["email"],
+            "city": data["city"]
+        }
+
+user_info_schema = UserInfoSchema()
+
--- api/api/book/models.py
+++ api/api/book/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class BookInfo(db.Model):
     uuid = db.Column(db.String(36), primary_key=True,
                      default=lambda: str(uuid4()))
-    # holderID = db.Column(db.Integer, nullable=False)
+    holderID = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(36), nullable=False)
     author = db.Column(db.String(36), nullable=False)
     genre = db.Column(db.String(36), nullable=False)
