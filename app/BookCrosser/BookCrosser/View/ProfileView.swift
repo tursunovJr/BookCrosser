8ecfdb062c890a717427131a41dbd39128750f8f
ztursunov <ztursunov@ivi.ru>
2023-04-16 15:37:33 +0300

    Profile page starting editing

--- app/BookCrosser/BookCrosser/View/ProfileView.swift
+++ app/BookCrosser/BookCrosser/View/ProfileView.swift
@@ -11,13 +11,36 @@ struct ProfileView: View {
     @EnvironmentObject private var authModel: AuthViewModel
     
     var body: some View {
+        
         VStack {
-            Text("Hello, \(authModel.user?.email ?? "Anonymous guest")")
+            HStack {
+                Spacer()
+                Text("Profile").bold().font(.system(size: 25.0)).foregroundColor(.white).frame(alignment: .center)
+                Spacer()
+            }
+            .frame(height: 50.0)
+            .background(Color.blue)
+            
+            Image(systemName: "figure.arms.open")
+                .frame(width: 100.0, height: 100.0, alignment: .center)
+                .background(Color.green)
+                .cornerRadius(50.0)
+                .padding()
+            
+//            Text("Hello, \(authModel.user?.email ?? "Anonymous guest")")
+            Text("Name Surname").font(.title)
+            
+            SettingsListRowView(model: .init(name: "Personal information")).frame(height: 40.0)
+            SettingsListRowView(model: .init(name: "Books")).frame(height: 40.0)
+            SettingsListRowView(model: .init(name: "Requests")).frame(height: 40.0)
+            SettingsListRowView(model: .init(name: "Reviews")).frame(height: 40.0)
+            
             Button {
-                authModel.signOut()
+//                authModel.signOut()
             } label: {
                 Text("Log Out").bold()
             }
+            Spacer()
 
         }
         
